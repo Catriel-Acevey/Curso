17a7a179deb20b76c593585a7333046b7dcc4393
Catriel-Acevey <56076984+Catriel-Acevey@users.noreply.github.com>
2019-10-30 12:19:12 -0300

    30/10 (fin tablear)

--- Calculate and Display Statistics/completarTablas.js
+++ Calculate and Display Statistics/completarTablas.js
@@ -19,13 +19,13 @@ function promedioDeVotosConPartido(array) {
     for (i = 0; i < array.length; i++) {
         promedio += array[i].votes_with_party_pct;
     }
-    promedio = promedio / (array.length + 1);
+    promedio = promedio / (array.length);
     promedio = promedio.toFixed([2]);
     return promedio;
 }
 
 function obtenerPartido(array) {
-    if(array.length == 0) return "";
+    if(!array) return null;
     for (i = 0; i < array.length; i++) {
         if (array[0].party != array[i].party) return "Todos";
     }
@@ -35,14 +35,34 @@ function obtenerPartido(array) {
 }
 
 function tablearFilaGlance(array) {
+    if(!array) return "";
     var caja = "<tr><td>" + obtenerPartido(array) + "</td><td>" + (array.length) + "</td><td>" + promedioDeVotosConPartido(array) + "%" + "</tr>";
     return caja;
 }
 
-function ordenarMissed(array) {
+function ordenarSenadores(array,campo,orden) {
+    if(!array) return array;
+    if(orden==1){
     array.sort(function (a, b) {
-        return ((a.missed_votes) - (b.missed_votes));
-    });
+        return ((a[campo]) - (b[campo]));
+    })
+    }else{
+    var aux=[];
+    for(var i=array.length-1;i>=0;i--){
+        aux.push(array[i]);
+    }
+       return aux;
+
+    }
+    return array;
+}
+function getOrderFilter(array,campo,orden,cant){
+    var pepito=ordenarSenadores(array,campo,orden);
+    var resultado=[];
+    for(var i=0;i<cant;i++){
+    resultado[i]=pepito[i];
+    }
+    return resultado;
 }
 
 function getName(string) {
@@ -53,42 +73,33 @@ function getName(string) {
     }
 }
 
-function tablearColumnaMostEngaged(array) {
-    var caja = "";
-    for (i = 0; i < (array.length * 0.1); i++) {
-        caja += ("<tr><td>" + getName(array[i].first_name) + " " + getName(array[i].middle_name) + " " + getName(array[i].last_name) + "</td><td>" + array[i].missed_votes + "</td><td>" + array[i].missed_votes_pct + "</tr>");
-    }
-    document.getElementById("tablaSenateMostEngaged").innerHTML = caja;
+function porcentajeACantidad(member,campo){
+    var porcentaje=parseInt(member[campo]);
+    if(porcentaje==0) return "0";
+    var cantidad=parseInt(member.total_votes)
+    cantidad=(cantidad*10)/porcentaje;
+    cantidad=cantidad.toFixed([0]);
+    return (cantidad.toString(10));
 }
 
-function tablearColumnaLeastEngaged(array) {
+function tablearColumnaMostEngaged(array,id,campo) {
     var caja = "";
     for (i = 0; i < (array.length * 0.1); i++) {
-        caja += ("<tr><td>" + getName(array[array.length - i - 1].first_name) + " " + getName(array[array.length - i - 1].middle_name) + " " + getName(array[array.length - i - 1].last_name) + "</td><td>" + array[array.length - i - 1].missed_votes + "</td><td>" + array[array.length - i - 1].missed_votes_pct + "</tr>");
+        caja += ("<tr><td>" + getName(array[i].first_name) + " " + getName(array[i].middle_name) + " " + getName(array[i].last_name) + "</td><td>" + porcentajeACantidad(array[i],campo) + "</td><td>" + array[i][campo] + "</td></tr>");
     }
-    document.getElementById("tablaSenateLeastEngaged").innerHTML = caja;
+    document.getElementById(id).innerHTML = caja;
 }
 
-if (estoyenhouse) {
-    senadoresH = dataHouse.results[0].members;
-    console.log(senadoresH);
+function tablearColumnaLeastEngaged(array,id,campo) {
+    var caja = "";
+    for (i = 0; i < (array.length * 0.1); i++) {
+        caja += ("<tr><td>" + getName(array[array.length - i - 1].first_name) + " " + getName(array[array.length - i - 1].middle_name) + " " + getName(array[array.length - i - 1].last_name) + "</td><td>" + porcentajeACantidad(array[array.length - i - 1],campo) + "</td><td>" + array[array.length - i - 1][campo] + "</tr>");
+    }
+    document.getElementById(id).innerHTML = caja;
+}
 
-    var democratasH = obtenerSenadores(senadoresH, "D");
-
-    var independientesH = obtenerSenadores(senadoresH, "I");
-    
-    var republicanosH = obtenerSenadores(senadoresH, "R");
-    console.log(independientesH);
-    document.getElementById("tablaHouseGlance").innerHTML = tablearFilaGlance(democratasH) + tablearFilaGlance(republicanosH) + (independientesH)?tablearFilaGlance(independientesH):"" + tablearFilaGlance(senadoresH);
-
-    ordenarMissed(senadoresH);
-
-    tablearColumnaMostEngaged(senadoresH);
-
-    tablearColumnaLeastEngaged(senadoresH);
-} else {
+if(estoyen == "senate-attendance"){
     senadores = dataSenate.results[0].members;
-    console.log(senadores);
 
     var democratas = obtenerSenadores(senadores, "D");
     var independientes = obtenerSenadores(senadores, "I");
@@ -96,9 +107,55 @@ if (estoyenhouse) {
 
     document.getElementById("tablaSenateGlance").innerHTML = tablearFilaGlance(democratas) + tablearFilaGlance(republicanos) + tablearFilaGlance(independientes) + tablearFilaGlance(senadores);
 
-    ordenarMissed(senadores);
+    ordenarSenadores(senadores,"missed_votes_pct",1);
 
-    tablearColumnaMostEngaged(senadores);
+    tablearColumnaMostEngaged(senadores,"tablaSenateMostEngaged","missed_votes_pct");
 
-    tablearColumnaLeastEngaged(senadores);
+    tablearColumnaLeastEngaged(senadores,"tablaSenateLeastEngaged","missed_votes_pct");
+} else if (estoyen == "house-attendance") {
+    senadoresH = dataHouse.results[0].members;
+
+    var democratasH = obtenerSenadores(senadoresH, "D");
+
+    var independientesH = obtenerSenadores(senadoresH, "I");
+    
+    var republicanosH = obtenerSenadores(senadoresH, "R");
+
+    document.getElementById("tablaHouseGlance").innerHTML = tablearFilaGlance(democratasH) + tablearFilaGlance(republicanosH) + tablearFilaGlance(independientesH) + tablearFilaGlance(senadoresH);
+
+    ordenarSenadores(senadoresH,"missed_votes_pct",1);
+
+    tablearColumnaMostEngaged(senadoresH,"tablaHouseMostEngaged","missed_votes_pct");
+
+    tablearColumnaLeastEngaged(senadoresH,"tablaHouseLeastEngaged","missed_votes_pct");
+} else if (estoyen == "senate-party"){
+    senadores = dataSenate.results[0].members;
+
+    var democratas = obtenerSenadores(senadores, "D");
+    var independientes = obtenerSenadores(senadores, "I");
+    var republicanos = obtenerSenadores(senadores, "R");
+
+    document.getElementById("tablaSenateGlance").innerHTML = tablearFilaGlance(democratas) + tablearFilaGlance(republicanos) + tablearFilaGlance(independientes) + tablearFilaGlance(senadores);
+
+    ordenarSenadores(senadores,"votes_with_party_pct",1)
+
+    tablearColumnaMostEngaged(senadores,"tablaSenateMostLoyal","votes_with_party_pct");
+
+    tablearColumnaLeastEngaged(senadores,"tablaSenateLeastLoyal","votes_with_party_pct");
+} else {
+    senadoresH = dataHouse.results[0].members;
+
+    var democratasH = obtenerSenadores(senadoresH, "D");
+
+    var independientesH = obtenerSenadores(senadoresH, "I");
+    
+    var republicanosH = obtenerSenadores(senadoresH, "R");
+
+    document.getElementById("tablaHouseGlance").innerHTML = tablearFilaGlance(democratasH) + tablearFilaGlance(republicanosH) + tablearFilaGlance(independientesH) + tablearFilaGlance(senadoresH);
+
+    ordenarSenadores(senadoresH,"votes_with_party_pct",1);
+
+    tablearColumnaMostEngaged(senadoresH,"tablaHouseMostLoyal","votes_with_party_pct");
+
+    tablearColumnaLeastEngaged(senadoresH,"tablaHouseLeastLoyal","votes_with_party_pct");
 }
